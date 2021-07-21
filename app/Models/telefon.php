f1d28f5e3dae00eb9419505cefdb1e773ce09f48
Han <najhan.mnajib@gmail.com>
2021-07-21 18:42:08 +0800

    models with capital letter

--- app/Models/telefon.php
+++ app/Models/telefon.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class telefon extends Model
+class Telefon extends Model
 {
     use HasFactory;
 }
