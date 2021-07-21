24aeba742c323df233c050b2787db5cbc87fe79f
Han <najhan.mnajib@gmail.com>
2021-07-21 13:01:17 +0800

    first commit

---
+++ database/factories/TelefonFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\telefon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TelefonFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = telefon::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
